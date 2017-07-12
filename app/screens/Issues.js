f17a5e3f237384b10778cea6dff244c0117cacb9
Nismit <nismit.dev@gmail.com>
2017-07-11 18:13:12 -0700

    Added expandable view

--- app/screens/Issues.js
+++ app/screens/Issues.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import { Card, Icon } from 'react-native-elements';
+import { ScrollView, StyleSheet, Text, View, } from 'react-native';
+import { Card, Icon, Button, } from 'react-native-elements';
 import moment from 'moment';
+import Collapsible from 'react-native-collapsible';
 
 export default class Issues extends Component {
   constructor( props ) {
     super( props )
+
+    const collapsed  = {}
+    this.props.navigation.state.params.issues.forEach((issue)=>collapsed[issue.id] = true)
+
     this.state = {
       loaded: false,
+      collapsed: collapsed,
     }
   }
 
@@ -15,6 +21,15 @@ export default class Issues extends Component {
 
   }
 
+  _handler(e) {
+    console.log(e);
+  }
+
+  _toggleExpanded = (id) => {
+    const newState = { collapsed: this.state.collapsed }
+    newState.collapsed[id] = !this.state.collapsed[id]
+    this.setState(newState)
+  }
 
   fetchData( URL, issueFlag ) {
     fetch(URL)
@@ -38,15 +53,24 @@ export default class Issues extends Component {
 
   render() {
     // let lastCommitTime = moment( pushed_at ).format('YYYY/MM/DD HH:mm')
-
     return (
       <ScrollView>
         { this.props.navigation.state.params.issues.map(( issue ) => (
           <Card key={issue.id}>
               <View>
-                <Text style = { styles.issueTitle }>{issue.title}</Text>
-                <Text>{issue.body}</Text>
+                <Text style = { styles.issueTitle }>{ issue.title }</Text>
+                <Text>{ issue.user.login } opened this issue.</Text>
               </View>
+
+              <Collapsible collapsed = { this.state.collapsed[issue.id] }>
+                <Text>{ issue.body }</Text>
+              </Collapsible>
+
+              <Button
+                buttonStyle = { styles.buttonStyle }
+                onPress = { ()=> this._toggleExpanded(issue.id) }
+                title = { this.state.collapsed[issue.id] ? 'SHOW ISSUE' : 'HIDE ISSUE' }
+              />
           </Card>
         ))}
       </ScrollView>
@@ -63,5 +87,9 @@ const styles = StyleSheet.create({
   issueTitle: {
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  buttonStyle: {
+    width: '100%',
+    marginTop: 15,
   }
 });
