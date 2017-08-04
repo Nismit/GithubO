2f3cb73d52b2310326b3e7460b90dc7e545189d7
Nismit <nismit.dev@gmail.com>
2017-08-03 23:39:59 -0700

    Sorted code

--- app/issues/IssueDetail.js
+++ app/issues/IssueDetail.js
@@ -1,8 +1,6 @@
-import moment from 'moment';
-import HTMLView from 'react-native-htmlview';
 import React, { Component } from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import { List, ListItem, Icon } from 'react-native-elements';
+import { Icon } from 'react-native-elements';
 import ListStyle from '../utils/styles/lists';
 import Loader from '../components/Loader/Loader';
 
